40cdd2fa0637ccefe19558b3af951c0ac7a85d20
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-01-17 23:09:09 +0900

    fix: update makefile

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 
 	"github.com/TOMOFUMI-KONDO/toy/ast"
+
 	"github.com/TOMOFUMI-KONDO/toy/interpreter"
 )
 
 func main() {
-	a := ast.NewAst()
 	// ( 1 - 2 * 3 ) + 4
-	exp := a.Add(
-		a.Subtract(
-			a.Integer(1),
-			a.Multiply(
-				a.Integer(2),
-				a.Integer(3),
+	exp := ast.NewAdd(
+		ast.NewSubtract(
+			ast.NewInteger(1),
+			ast.NewMultiply(
+				ast.NewInteger(2),
+				ast.NewInteger(3),
 			),
 		),
-		a.Integer(4),
+		ast.NewInteger(4),
 	)
 
 	i := interpreter.NewInterpreter()
