8f0d71bd3e836f7cfcbbbe94f981ede57e118567
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-03 18:32:27 +0900

    fix: change method to pass toy program to toy interpreter

--- cmd/toy/main.go
+++ cmd/toy/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 
@@ -11,12 +10,18 @@ import (
 )
 
 func main() {
-	b, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("failed to read stdin: %v", err)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "toy file path must be passed.")
+		os.Exit(1)
 	}
 
-	toy := &parser.Toy{Buffer: string(b)}
+	path := os.Args[1]
+	input, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("failed to read file %q: %v", path, err)
+	}
+
+	toy := &parser.Toy{Buffer: string(input)}
 	if err := toy.Init(); err != nil {
 		panic(err)
 	}
